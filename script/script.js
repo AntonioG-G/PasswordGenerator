49235ad17353b5907758620a5b4bd571d6b69d5f
Antonio Guerrero <antonioguerrero2109@gmail.com>
2023-05-27 08:48:32 -0600

    Funcionalidades terminadas: copiar, ocultar, y mostrar. Creación de css secundario para switch

--- script/script.js
+++ script/script.js
@@ -1,25 +1,64 @@
-const characters = [
-    low = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-    caps = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-    numbers = ['1','2','3','4','5','6','7','8','9','0'],
-    special = ['$','%','&','-','_','ñ','?','!']
-];
-let config = [0];
-let long = 0;
+const characters = {
+    low: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    caps: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    special: ['$', '%', '&', '-', '_', 'ñ', '?', '!']
+  };
 
 const form = document.getElementById('passwordForm');
-
-form.addEventListener('submit', function(event) { 
-    event.preventDefault();
-    console.log('event');
-});
-
-const generate = () =>{
+const result = document.getElementById('result');
+  
+    form.addEventListener('submit', function(event) { 
+        event.preventDefault();
+        const long = form.elements.optionSelect.value;
+        const uppercase = form.elements.switchCaps.checked;
+        const numbers = form.elements.switchNumbers.checked;
+        const specialChars = form.elements.switchSpecials.checked;
+        
+        generate(uppercase, numbers, specialChars,long);
+    });
+  
+  function generate(caps, numbers, specials,length) {
     let password = [];
-    for (let i = 0; i <= long; i++) {
-        let random = config[Math.floor(Math.random() * config.length)];
-        let random2 = characters[random][Math.floor(Math.random() * characters[random].length)]
-        password.push(random2);
+    const config = [];
+    config.push('low');
+    if (caps) {
+      config.push('caps');
     }
-    console.log(`Tu contraseña es: ${password.join('')}`);
-}
+    
+    if (numbers) {
+      config.push('numbers');
+    }
+    
+    if (specials) {
+      config.push('special');
+    }
+    
+    for (let i = 0; i <= length; i++) {
+      const randomConfig = config[Math.floor(Math.random() * config.length)];
+      const randomChar = characters[randomConfig][Math.floor(Math.random() * characters[randomConfig].length)];
+      password.push(randomChar);
+    }
+    const passwordInput = document.getElementById('result');
+    passwordInput.type = 'password';
+    result.value = password.join('');
+  }
+
+  function revealPassword() {
+    const passwordInput = document.getElementById('result');
+    if (passwordInput.type == 'password') {
+        passwordInput.type = 'text';
+    }else if (passwordInput.type == 'text') {
+        passwordInput.type = 'password';
+    }
+  }
+  
+  function copyToClipboard() {
+    
+    navigator.clipboard.writeText(result.value)
+    .then(()=>{
+        alert('Contraseña copiada al portapapeles');
+    }).catch(err => alert("Error al copiar la contraseña: ", err))
+    
+  }
+  
